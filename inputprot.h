7da4ea28d1623d2796148190583c15de4a9f1326
Sxllaire <space.shundere@gmail.com>
2025-09-22 13:46:46 -0500

    Create inputprot.h

---
+++ inputprot.h
@@ -0,0 +1,21 @@
+#ifndef INPUTPROT_H
+#define INPUTPROT_H
+
+#include <string>
+using namespace std;
+
+// Both bounds
+long input_protect(const string& prompt, const string& error, long lower, long upper);
+double input_protect(const string& prompt, const string& error, double lower, double upper);
+
+// Lower bound
+long input_protect(const string& prompt, const string& error, long lower);
+double input_protect(const string& prompt, const string& error, double lower);
+
+// Upper bound
+long input_protect_upper(const string& prompt, const string& error, long upper);
+double input_protect_upper(const string& prompt, const string& error, double upper);
+
+char input_protect(const string& prompt, const string& error, const string& validChars);
+
+#endif
