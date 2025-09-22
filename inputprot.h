b18f346d1657261256cd0a51e12096bc57eb9581
Sxllaire <space.shundere@gmail.com>
2025-09-22 13:48:53 -0500

    Update inputprot.h

--- inputprot.h
+++ inputprot.h
@@ -1,3 +1,13 @@
+//*************************************************************
+//
+//Danil Alieinikov
+//H00880905
+//CSC122-01
+//
+//I certify that this is my work and where appropriate an extension
+//of the starter code provided by the assignment
+//*************************************************************
+
 #ifndef INPUTPROT_H
 #define INPUTPROT_H
 
