75035132a0ee035f93881ab9921f16f5ae6ced07
Sxllaire <space.shundere@gmail.com>
2025-09-22 13:44:07 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//*************************************************************
+//
+//Danil Alieinikov
+//H00880905
+//CSC122-01
+//
+//I certify that this is my work and where appropriate an extension
+//of the starter code provided by the assignment
+//*************************************************************
